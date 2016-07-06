e28b398577ef1d6c09d85c0ff2e7334b9226e05f
craig1123 <craigwalker1123@gmail.com>
2016-07-06 17:57:44 -0600

    for kicks

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -13,7 +13,7 @@ module.exports = {
 
   me: function(req, res, next) {
     console.log(req.user);
-    if (!req.user) return res.status(401).send('current user not defined');
+      if (!req.user) return res.status(401).send('current user not defined');
     req.user.password = null;
     return res.status(200).json(req.user);
   },
